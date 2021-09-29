d54aa447dc6f4ff01a6ac56432f9359459c52476
Manjit Shakya <manjitshakya@Manjit.local>
2021-09-29 20:10:18 +0545

    minor changes

--- src/main/java/com/nepalaya/threadbasics2/Problem.java
+++ src/main/java/com/nepalaya/threadbasics2/Problem.java
@@ -2,26 +2,26 @@ package com.nepalaya.threadbasics2;
 
 public class Problem {
 
-    public static final String resource1 = "resource1";
-    public static final String resource2 = "resource2";
+    public static final String resource1 = "Resource-1";
+    public static final String resource2 = "Resource-2";
 
 
     public static void main(String args[]){
 
         Thread thread1 = new Thread(() -> {
             synchronized (resource1){
-                System.out.println(resource1+" is accessed !");
+                System.out.println(resource1+" is accessed by "+Thread.currentThread().getName());
                 synchronized (resource2){
-                    System.out.println(resource2+ "is accessed !");
+                    System.out.println(resource2+ "is accessed by "+Thread.currentThread().getName());
                 }
             }
         });
 
         Thread thread2 = new Thread(() -> {
             synchronized (resource2){
-                System.out.println(resource2+" is accessed !");
+                System.out.println(resource2+" is accessed by "+Thread.currentThread().getName());
                 synchronized (resource1){
-                    System.out.println(resource1+ "is accessed !");
+                    System.out.println(resource1+ "is accessed by "+Thread.currentThread().getName());
                 }
             }
         });
